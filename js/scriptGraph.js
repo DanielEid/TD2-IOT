c36ae509abd6f6968577191e60513a329c8403b3
Daniel Eid <danieleid06@gmail.com>
2019-02-11 17:47:38 +0100

    Update sur graph

--- js/scriptGraph.js
+++ js/scriptGraph.js
@@ -1,8 +1,13 @@
 function initialiseGraph(dataTime,dataTemp){
 
 console.log(dataTemp);
-  var dom = document.getElementById("containerGraph");
-    dom.style="height:300px;"
+
+//Perso: permet de supprimer le graph et de le refaire (pour l'update des données)
+  var container=document.querySelector("#containerGraph");
+  container.innerHTML='<div id=\"graphTemp\"></div>';
+//  
+  var dom = document.getElementById("graphTemp");
+  dom.style="height:300px;"
   var myChart = echarts.init(dom);
   var app = {};
   option = null;
